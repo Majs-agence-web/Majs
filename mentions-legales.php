676be94bbd71f7e01c3832cbcf8610206e8ccd84
MLespagnol <maxim590@hotmail.fr>
2021-03-10 12:05:03 +0100

    Ajout PDC + ML

---
+++ mentions-legales.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/majs-favicon-128x108.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+  
+
+</head>
+<body>
+
+<?php 
+        include("header.php")
+    ?>
+  
+  <section class="cid-srfeqb2KVS mbr-parallax-background" id="header02-0">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(75, 101, 112);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-start">
+            <div class="mbr-white col-12 text-center mx-auto">
+                <h1 class="mbr-section-title mbr-regular pb-3 align-center mbr-fonts-style display-1">Mentions légales</h1>
+                
+                
+                
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+<section class="header4 cid-srfeqL2qWg" id="content05-1">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site internet www.majs.fr.
+<br><br>L’identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
+<br>- Propriétaire et éditeur : SASU MAJS WEB AGENCY - 59 Avenue Roger Salengro 59170 CROIX<br>- N° de TVA :&nbsp;<br>- Responsable publication et Webmaster : MAJS WEB AGENCY - 59 Avenue Roger Salengro 59170 CROIX
+<br>- N° de téléphone : 07 70 39 27 93<br>- Email : contact@majs.fr<br>- Hébergeur : IONOS - 7 Place de la Gare, 57200 Sarreguemines<br></p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfeqZxRAa" id="content05-2">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">1. Conditions générales d’utilisation du site et des services proposés.</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">Le Site constitue une œuvre de l’esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables.
+<br>Le Client ne peut en aucune manière réutiliser, céder ou exploiter pour son propre compte tout ou partie des éléments ou travaux du Site.
+<br>L’utilisation du site www.majs.fr implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs sont donc invités à les consulter de manière régulière.
+<br>Ce site internet est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par l’éditeur.
+<br>Le site web est mis à jour régulièrement par l’éditeur qui en est responsable. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srferKLfCy" id="content05-3">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">2. Description des services fournis.</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">Le site internet www.majs.fr est édité selon une démarche d’information et présente l’ensemble des activités de la société qui s’efforce de fournir des éléments aussi précis que possible. Toutefois, il ne pourra être tenu responsable des oublis, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
+<br>Toutes les informations indiquées sur le site www.majs.fr sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfes4ejjW" id="content05-4">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">3. Limitations contractuelles sur les données techniques.</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">Le site utilise la technologie html.
+<br>L’éditeur ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur à-jour.
+<br>Le site www.majs.fr est hébergé chez un prestataire sur le territoire de l’Union Européenne conformément aux dispositions du Règlement Général sur la Protection des Données (RGPD : n° 2016-679)</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfespQier" id="content05-5">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">4. Propriété intellectuelle et contrefaçons.</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">MAJS WEB AGENCY est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, icônes et sons.
+<br>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l’éditeur.
+<br>Toute exploitation non autorisée du site ou de l’un des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfesJsTHy" id="content05-6">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">5. Limitations de responsabilité.</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">
+                    MAJS WEB AGENCY agit en tant qu’éditeur du site et est responsable de la qualité et de la véracité du contenu qu’il publie.</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfet3PKhx" id="content05-7">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">6. Gestion des données personnelles.</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">
+                    L’utilisateur est informé des réglementations concernant la communication marketing, la loi du 21 Juin 2014 pour la confiance dans l’Economie Numérique, la Loi Informatique et Liberté du 06 Août 2004 ainsi que du Règlement Général sur la Protection des Données (RGPD : n° 2016-679).</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfetoFBWS" id="content05-8">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">6.1 Responsable de la collecte des données personnelles</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">Pour les Données Personnelles collectées dans le cadre d’une navigation sur le Site, le responsable du traitement des Données Personnelles est la société MAJS WEB AGENCY.
+<br>En tant que responsable du traitement des données qu’il collecte, le site www.majs.fr s’engage à respecter le cadre des dispositions légales en vigueur. Il lui appartient notamment d’établir les finalités de ses traitements de données, de fournir à ses prospects et clients, à partir de la collecte de leurs consentements, une information complète sur le traitement de leurs données personnelles et de maintenir un registre des traitements conforme à la réalité.</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfetFobja" id="content05-9">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">6.2 Finalité des données collectées</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">- Permettre la navigation sur le Site
+<br>- Prévenir et lutter contre la fraude informatique (spamming, hacking)
+<br>- Améliorer la navigation sur le Site
+<br>MAJS WEB AGENCY ne commercialise pas vos données personnelles qui sont donc utilisées uniquement à fin de statistiques et d’analyses.</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfetToAGW" id="content05-a">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">6.3 Droit d’accès, de rectification et d’opposition</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">Conformément à la réglementation européenne en vigueur, le visiteur du site www.majs.fr dispose des droits suivants :
+<br>- Droit d’accès (article 15 RGPD) et de rectification (article 16 RGPD), de mise à jour, de complétude des données des Utilisateurs, droit de verrouillage ou d’effacement des données des Utilisateurs à caractère personnel (article 17 du RGPD), lorsqu’elles sont inexactes, incomplètes, équivoques, périmées, ou dont la collecte, l’utilisation, la communication ou la conservation est interdite
+<br>- Droit de retirer à tout moment un consentement (article 13-2c RGPD)
+<br>- Droit à la limitation du traitement des données des Utilisateurs (article 18 RGPD)
+<br>- Droit d’opposition au traitement des données des Utilisateurs (article 21 RGPD)
+<br>- Droit à la portabilité des données que les Utilisateurs auront fournies, lorsque ces données font l’objet de traitements automatisés fondés sur leur consentement ou sur un contrat (article 20 RGPD)
+<br>Si le visiteur souhaite savoir comment www.majs.fr utilise ses Données Personnelles, demander à les rectifier ou s’oppose à leur traitement, l’Utilisateur peut contacter l’éditeur par écrit à l’adresse suivante : SASU MAJS WEB AGENCY – 59 Avenue Roger Salengro 59170 CROIX (Joindre au courrier une pièce d’identité).
+<br>Les demandes de suppression de Données Personnelles seront soumises aux obligations qui sont imposées à l’éditeur par la loi, notamment en matière de conservation ou d’archivage des documents. Enfin, les visiteurs du site www.majs.fr peuvent déposer une réclamation auprès des autorités de contrôle, et notamment de la CNIL (https://www.cnil.fr/fr/plaintes).</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="header4 cid-srfeugGRuo" id="content05-b">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class=" col-md-12 col-lg-10 align-left">
+                
+                <h1 class="mbr-section-title align-left mbr-white pb-2 mbr-fonts-style display-5">6.4 Non-communication des données personnelles</h1>
+
+                <p class="mbr-text align-left mbr-fonts-style display-7">
+                    L’éditeur s’interdit de traiter, héberger ou transférer les Informations collectées sur ses Clients vers un pays situé en dehors de l’Union européenne ou reconnu comme « non adéquat » par la Commission européenne sans en informer préalablement le client. Pour autant, l’éditeur reste libre du choix de ses sous-traitants techniques et commerciaux à la condition qu’il présentent les garanties suffisantes au regard des exigences du Règlement Général sur la Protection des Données (RGPD : n° 2016-679).</p>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+
+<section class="clients cid-sp5c05Igt8" id="clients01-5d">
+    
+
+    
+
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1380px" height="810px" viewBox="0 -8.992806499463768e-14 1380 810" preserveAspectRatio="xMidYMid meet">
+        <rect id="svgEditorBackground" x="0" y="0" width="1380" height="810" style="fill: none; stroke: none;"></rect>
+        <defs id="svgEditorDefs">
+            <polygon id="svgEditorShapeDefs" style="fill:khaki;stroke:black;vector-effect:non-scaling-stroke;stroke-width:1px;"></polygon>
+            <path id="svgEditorClosePathDefs" style="stroke:black;stroke-width:1px;fill:khaki;"></path>
+        </defs>
+        <path d="M15083.791600683318,645.6720847341486c496.6000000000058,747.7999999999951,1953.3704016668416,1862.6734725832403,2751.999999999978,1967.9999999999955s1884.300000000003,-198.89999999999964,2718.7056474818382,-552.6365690040448s1746.4898225846773,-519.9524548776521,2593.2943525181545,-567.3634309959566s2251,-23.399999999999636,4384,1520.0000000000032s3205.199999999997,1437.2000000000025,4047.9999999999854,1520.0000000000018s3305.000000000029,-270.2999999999993,4640.000000000029,-1152s74.60000000000582,3328.3000000000065,32,3792.0000000000073s-19509.4,4591.000000000004,-21087.999999999993,-128.00000000000182s-1679.5999999999913,-6358.300000000001,-80,-6400Z" style="stroke:black;fill:khaki;stroke-width:0px;" id="e12_areaS3" transform="matrix(0.0650006 -0.00123765 0.00123765 0.0650006 -995.543 317.589)"></path>
+        <path d="M12434.469938292996,-979.2324603008726c496.600000000004,747.8000000000001,1953.3704016668398,1862.673472583247,2752.000000000018,1967.9999999999995s1884.2999999999865,-198.90000000000055,2718.7056474818364,-552.6365690040473s1746.4898225846846,-519.9524548776479,2593.2943525181618,-567.3634309959524s2251,-23.400000000001455,4384,1520s3205.199999999997,1437.1999999999962,4048,1519.9999999999955s3304.9999999999927,-270.3000000000011,4640,-1151.9999999999955s74.60000000000582,3328.3,32,3791.9999999999955s-19509.400000000023,4590.999999999997,-21088.000000000015,-127.99999999999818s-1679.5999999999985,-6358.300000000008,-80,-6400.0000000000055Z" style="stroke:black;fill:aqua;stroke-width:0px;" id="e19_areaS3" transform="matrix(0.0647268 0.00608646 -0.00608646 0.0647268 -823.261 331.266)"></path><text dy="-0.5em" style="fill:black;font-family:Arial;font-size:20px;" id="e14_texte" transform="matrix(0.0640454 0 0 0.0640454 -832.187 329.878)">
+            <textPath id="e13_textPath" xlink:href="#e12_areaS3">T</textPath>
+        </text>
+    </svg>
+</section>
+
+<?php 
+        include("footer.php")
+    ?>
+
+
+
+<script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/mbr-clients-slider/mbr-clients-slider.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+  
+
+</body>
+</html>
