37a156d5b34dadcea85f9eb15d253f6a4eed4b2f
MLespagnol <maxim590@hotmail.fr>
2021-03-17 15:06:48 +0100

    Modif Articile Aide Etat

--- Aide-etat.php
+++ Aide-etat.php
@@ -52,26 +52,28 @@
             </div>
             
             <div class="carousel-inner">
-                
-              
-            <div class="carousel-item">
+                <div class="carousel-item">
                     <div class="back"> </div>
                     <div class="media-container-row">
-                        
                         <div class="col-lg-9 col-12 image-element align-self-stretch">
                                 <img src="assets/images/mbr-1665x1250.jpg" alt="">
                         </div>
                         
                         <div class="col-lg-5 col-12">
-                        <div class="card-wrap">
-                    <h1 class="mbr-section-title align-center mbr-fonts-style display-5">Quelles sont les conditions pour toucher l’aide de 500 € à la création d’un site internet ?</h1>
-                    
-                            <p class="mbr-fonts-style block-quote display-7" style="text-align:justify;">Durant le 2e confinement, l’État a décidé de soutenir les entreprises via une aide financière à hauteur de 500 € pour la création d’un site internet (plus d’explications sur le dispositif ici). En effet, face au constat de l’énorme perte financière de certains commerçants à cause de l’épidémie de Covid-19. <br><br>Donc, pour limiter cette perte financière, la solution des commerçants est de digitaliser leur commerce en réalisant de la vente en ligne avec retrait des achats en Click &amp; Collect. Le problème beaucoup d’entreprises n’ont pas de site internet pour mettre en place rapidement cette solution.<br></p>
-                    
+                            <div class="card-wrap">
+                                <h1 class="mbr-section-title align-center mbr-fonts-style display-5">
+                                    Aide de 500 € attribuée pour la création d’un site internet : au delà du geste, la  réalité de la dépense
+                                </h1>
+                                <p class="mbr-fonts-style block-quote display-7" style="text-align:justify;">
+                                    Durant le deuxième confinement décrété fin octobre 2020, le gouvernement français a décidé de soutenir les entreprises souhaitant créer un 
+                                    site internet, en leur octroyant une aide financière à hauteur de 500€, dans le respect de quelques conditions cependant 
+                                    (plus d’explications sur le dispositif ici).  
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-      </div>
-                </div></div>
 
             
         </div>
@@ -85,27 +87,61 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10 col-sm-12 text-section">
-                <p class="mbr-fonts-style mbr-text align-center display-7" style="text-align:justify;">La somme devrait couvrir une partie des frais engagés pour la création d’un site internet type vitrine ou e-commerce. <br><br>L’achat d’un site internet peut être couteux, c’est pour cela que chez Majs, nous avons décidé de mettre en place une solution de la location avec option d’achat. Par exemple sur un site vitrine en location, cela représente 10 mois de location ou sur un site e-commerce cela couvre un peu plus de 6 mois de location.
-<br><br>Un site internet est un outil qui est intéressant même au-delà de la crise, car cela permet d’agrandir sa zone de chalandise via la livraison, mais aussi de garder un contact avec leur clientèle. <br><br>Cette subvention est une formidable occasion pour les PME françaises de rattraper leur retard versus aux autres pays européens qui ont un bien meilleur taux d’équipement en site internet.&nbsp;&nbsp;<br></p>
-               
+                <p class="mbr-fonts-style mbr-text align-center display-7" style="text-align:justify;">
+                    D’une part, cette subvention a pour vocation de permettre de combler une partie des pertes de chiffre d’affaires des commerçants et 
+                    entrepreneurs dans un contexte de crise sanitaire imposant notamment une fermeture physique des locaux commerciaux afin de respecter les 
+                    gestes barrière.
+                    <br>
+                    <br>D’autre part, elle va aussi - et surtout ! - permettre de pouvoir couvrir une partie des frais engagés par le commerçant et l’artisan dans 
+                    la création d’une “vitrine” internet, le fameux “site internet pour le e-commerce” résultant de la numérisation des opérations 
+                    vendeur-client-fournisseur-livreur…
+                    <br>
+                    <br>Car après l’expérience financière déplorable induite par le confinement imposé entre mars et mai 2020, les acteurs du commerce ont 
+                    très vite compris qu’il leur était d’un intérêt primordial, sinon vital, de digitaliser leur outil de travail, en faisant migrer la 
+                    vente directe vers la vente en ligne, avec retrait des achats en “click and collect”, pour citer l’opération la plus connue car 
+                    fondamentalement nécessaire et utilisée.
+                    <br>
+                    <br>Force est de constater que beaucoup d’entreprises se sont alors trouvées confrontées à des problèmes majeurs pour la mise en place 
+                    rapide d’une solution numérique, car concevoir, développer, exploiter et maintenir un site internet réclame, pour le néophyte qui s’y 
+                    attelle, des compétences pointues en codage informatique et en connaissance des réseaux notamment, bien au delà de la simple acquisition 
+                    du matériel (PC, location de l’hébergeur, etc... )
+                    <br>
+                    <br>Les prestataires numériques qui confectionnent et livrent au commerçant son site de e-commerce, en l’aidant si besoin à 
+                    l’exploiter et à le développer, sont nombreux sur le marché, le choix est donc large et certainement adapté à chaque besoin. 
+                    Mais un problème de taille est alors soulevé:  le ticket d’entrée de cette migration vers les outils de commerce numérique est 
+                    parfois si onéreux qu’il peut rester carrément inaccessible pour une grande majorité de postulants à la numérisation de leur business! 
+                    <br>
+                    <br>C’est en partant de ce constat que chez Majs nous avons décidé de mettre en place des solutions d’achat ou de location avec option d’achat. 
+                    Nous avons décidé de créer  des offres claires qui permettent de répondre à 3 cas de figure très précis: 
+                    <br>- Le site <a href="landing-page.php">“landing page” </a> (one page) qui permet de présenter rapidement sont activités.
+                    <br>- Le site <a href="site-vitrine.php">“vitrine”</a> permet de développer en plusieurs onglets la présentation de ses activités.
+                    <br>- Le site <a href="site-e-commerce.php">“e-commerce”</a> véritable boutique en ligne qui permet de vendre soit via la livraison, ou le click & collect, soit via les deux.
+                    <br>
+                    <br>Nous proposons également des services de location avec option d’achat, les sites en location peuvent être éligibles à la subvention du 
+                    gouvernement. Pour donner quelques exemples, pour un site vitrine, l’aide de l’Etat couvre environ 10 mois de location, pour un site de 
+                    e-commerce, cela couvre 6 mois de location.  Voici <a href="tarifs.php">nos tarifs</a>
+                    <br>
+                    <br>Au-delà des solutions standard proposées au catalogue, chez Majs, le client qui expose des besoins spécifiques est écouté. Notre
+                    mission est d’obtenir sa satisfaction, par une réalisation efficace, après une étude approfondie et professionnelle de ses besoins avec lui. 
+                    Notre politique tarifaire lisible et bien documentée permettra aussi d’éviter les “surprises” de dernière minute, sur la base d’engagements 
+                    respectueux des détails qui inspireront confiance, sécurité et sûreté. Évidemment, un site internet est un outil d’un grand intérêt, même 
+                    dans un contexte hors crise sanitaire: contact facilité avec les clients, tant au niveau des commandes, des livraisons et des prospects, 
+                    réduction des coûts publicitaires et de chalandise, la liste des avantages est très longue…! 
+                    <br>
+                    <br>Souhaitons pour conclure cet article que cette subvention puisse également permettre à plus ou moins court terme aux PME françaises de 
+                    rattraper leur retard par rapport à leurs homologues des autres pays européens, pour qui l’implantation en sites internet se situe à un 
+                    taux nettement plus élevé.  
+                </p>
             </div>
         </div>
     </div>
 </section>
 
 <section class="cid-sptVz2gjpD" id="title01-6d">
-
-    
-
-    
-
     <div class="container align-center">
         <div class="row justify-content-md-start">
-            <div class="mbr-white col-sm-12 col-md-10 col-lg-7">
-                <h1 class="mbr-section-title mbr-regular align-left mbr-fonts-style display-2">Qui peut bénéficier de cette aide ?</h1>
-                
-                
-                
+            <div class="mbr-white col-sm-12 col-md-10 col-lg-10">
+                <h1 class="mbr-section-title mbr-regular align-left mbr-fonts-style display-2">Qui peut bénéficier de cette aide ? (...dans la limite des crédits disponibles)</h1>
             </div>
         </div>
     </div>
@@ -113,47 +149,50 @@
 
 <section class="mbr-section extContent cid-sptTklljW4" id="extContent15-6a">
 
-    
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10 col-sm-12 text-section">
-                <p class="mbr-fonts-style mbr-text align-center display-7" style="text-align:justify;">L’État français a mis en place une aide exceptionnelle à la numérisation des entreprises. <br>Mais il faut répondre à quelques conditions :
-<br><br>-	Être une entreprise de moins de 11 salariés qui ont été fermé pendant le confinement de novembre 2020 ou qui sont des hôtels.
-<br>-	Les commerçants ou artisans ayant subi une interdiction d’accueil du public lors du confinement de novembre 2020 et certains hôtels (hébergements similaires), avec un chiffre d'affaires annuel inférieur à 2 millions d'euros HT et moins de 11 salariés, pourront en bénéficier, dans la limite des crédits disponibles.
-                    <br><br>Voir la liste des entreprises éligibles : <a class="texteIci" href="https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000043059724">ici</a>&nbsp;<br></p>
-              
+                <p class="mbr-fonts-style mbr-text align-center display-7" style="text-align:justify;">
+                    L’État français a mis en place une aide exceptionnelle à la numérisation des entreprises.
+                    <br>
+                    <br>Les conditions d’attribution concernent : 
+                    <br>- 1° Elles emploient moins de onze salariés. Ce seuil est calculé selon les modalités prévues par le I de l'article L. 130-1 du code de la sécurité sociale ;
+                    <br>- 2° Elles ont débuté leur activité avant le 30 octobre 2020 ;
+                    <br>- 3° Elles ont fait l'objet d'une interdiction d'accueil du public à partir du 30 octobre 2020 en application du décret du 29 octobre 2020 susvisé. Cette condition ne s'applique pas aux personnes qui exercent leur activité principale dans le secteur des hôtels et hébergements similaires ;
+                    <br>- 4° Elles sont inscrites au registre du commerce et des sociétés ou au répertoire des métiers ;
+                    <br>- 5° Elles sont à jour de leurs obligations à l'égard de l'administration fiscale et de l'organisme de recouvrement des cotisations patronales de sécurité sociale ;
+                    <br>- 6° Elles n'ont pas été déclarées en situation de liquidation judiciaire au jour de la demande d'aide ;
+                    <br>- 7° Elles ont un chiffre d'affaires annuel ou un total de bilan n'excédant pas 2 millions d'euros HT ;
+                    <br>- Pour les entreprises créées postérieurement au 30 octobre 2019 et n'ayant pas encore clos leur exercice comptable au 30 octobre 2020, le chiffre d'affaires de référence se calcule par rapport au chiffre d'affaires mensuel moyen sur la période comprise entre la date de création de l'entreprise et le 30 octobre 2020 ;
+                    <br>- 8° Lorsqu'elles sont constituées sous forme d'association, elles sont assujetties aux impôts commerciaux ou elles emploient au moins un salarié. 
+                    Retrouvez le décret <a class="texteIci" href="https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000043059724">ici</a>.
+                </p> 
             </div>
         </div>
     </div>
 </section>
 
 <section class="cid-sptWZPFavM" id="title01-6e">
-
-    
-
-    
-
     <div class="container align-center">
         <div class="row justify-content-md-start">
             <div class="mbr-white col-sm-12 col-md-10 col-lg-12">
-                <h1 class="mbr-section-title mbr-regular align-left mbr-fonts-style display-2">Comment obtenir les 500 euros de l’État pour la réalisation d’un site internet ?</h1>
-                
-                
-                
+                <h1 class="mbr-section-title mbr-regular align-left mbr-fonts-style display-2">Comment obtenir les 500 euros de l’État pour la réalisation d’un site internet ?</h1>  
             </div>
         </div>
     </div>
 </section>
 
 <section class="mbr-section extContent cid-sptX6eku83" id="extContent15-6g">
-
-    
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10 col-sm-12 text-section">
-                <p class="mbr-fonts-style mbr-text align-center display-7" style="text-align:justify;">Le chèque numérique d’une valeur de 500 euros sera alloué aux entreprises éligibles ayant effectué une demande directement sur le site officiel de l’Agence de Services et de Paiements (ASP). <br><br>Trouver le lien <a class="texteIci" href="https://cheque.francenum.gouv.fr/ecom/pre-requis">ici</a>, le montant sera versé directement sur le compte du bénéficiaire sur présentation de la facture:&nbsp;&nbsp;&nbsp;<br></p>
+                <p class="mbr-fonts-style mbr-text align-center display-7" style="text-align:justify;">
+                    Le chèque numérique d’une valeur de 500 euros sera alloué aux entreprises éligibles ayant effectué une demande directement sur le site officiel de 
+                    l’Agence de Services et de Paiements (ASP).
+                    <br>
+                    <br>Pour effectuer la demande, le lien est <a class="texteIci" href="https://cheque.francenum.gouv.fr/ecom/pre-requis">ici</a>, le montant sera versé directement sur le compte du bénéficiaire sur présentation de la facture émise par 
+                    le prestataire de services (...comme chez Majs, évidemment!) 
+                </p>
                <div class="py-3 ornament text-center">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 133 20" height="20" width="150" fill="#355665">
                         <g><path d="M127.18,6.19C121-.05,109.94-.24,101.84.1,90.84.56,80,3.3,69.32,5.77c-8,1.86-16,3.63-24,5.72-9.21,2.41-18.6,4.72-28.14,5.19-4.1.2-8.48,0-12.16-2a6.6,6.6,0,0,1-2.7-9A5.46,5.46,0,0,1,7.07,2.77c4-.22,6.73,8.39-.08,4.58a.49.49,0,0,0-.49.84c4.88,3,10.8,0,5.4-4.91C7.6-.63.4,2.06,0,8-1,24,28.25,17,36.29,15.24,53.74,11.37,70.9,6.44,88.53,3.33c11.1-2,24.75-3.54,35.06,2.19a16,16,0,0,1,6.86,8.85c1.29,4.31-4.35,4.2-7,3.52-4.84-1.26-2.42-8,1.22-3,.61.83,2,0,1.4-.82-2.15-2.93-6.71-4.9-7.12.35s8.36,7.24,11.72,4.47C135,15.32,129.91,9,127.18,6.19Z"></path></g>
